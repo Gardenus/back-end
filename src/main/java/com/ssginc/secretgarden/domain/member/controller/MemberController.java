83683c21916fa34034027aca36c4f87c55f55d57
HaejuneLee <gowns0205@gmail.com>
2024-01-25 17:27:38 +0900

    feat: swagger 3.0 적용

--- src/main/java/com/ssginc/secretgarden/domain/member/controller/MemberController.java
+++ src/main/java/com/ssginc/secretgarden/domain/member/controller/MemberController.java
@@ -1,4 +1,16 @@
 package com.ssginc.secretgarden.domain.member.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class MemberController {
+
+    @GetMapping("/test")
+    public String test(){
+        return "success";
+    }
+
+
 }
--- src/main/java/com/ssginc/secretgarden/domain/member/entity/Member.java
+++ src/main/java/com/ssginc/secretgarden/domain/member/entity/Member.java
@@ -1,4 +1,14 @@
 package com.ssginc.secretgarden.domain.member.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "member")
 public class Member {
+    @Id
+    private Long id;
 }
