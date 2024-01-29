06f408f38b2286f7080ddb6d4f6fb9019b0f8985
iamneveralone <fieldsawarded@naver.com>
2024-01-29 16:58:48 +0900

    feat: 축하 리스트 조회 API 구현

---
+++ src/main/java/com/ssginc/secretgarden/domain/celebration/dto/DetailCelebrationDto.java
@@ -0,0 +1,37 @@
+package com.ssginc.secretgarden.domain.celebration.dto;
+
+import com.ssginc.secretgarden.domain.celebration.dto.response.CelebrationResponseDto;
+import com.ssginc.secretgarden.domain.celebration.entity.Celebration;
+import com.ssginc.secretgarden.domain.member.entity.Member;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+
+import java.time.LocalDateTime;
+
+@Getter @Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class DetailCelebrationDto {
+    private Integer id;
+    private String writer;
+    private String company;
+    private String title;
+    private String content;
+    private String category;
+    private Boolean isSecret;
+    private LocalDateTime createdAt;
+
+    public static DetailCelebrationDto toDto(Celebration celebration, Member member){
+        return DetailCelebrationDto.builder()
+                .id(celebration.getId())
+                .writer(member.getName())
+                .company(member.getCompany().getName())
+                .title(celebration.getTitle())
+                .content(celebration.getContent())
+                .category(celebration.getCategory())
+                .isSecret(celebration.getIsSecret())
+                .createdAt(celebration.getCreatedAt())
+                .build();
+    }
+}
