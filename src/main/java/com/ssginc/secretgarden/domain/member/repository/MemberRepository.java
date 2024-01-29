511ca5434b4c446121e5906f591689ae91b97ed8
iamneveralone <fieldsawarded@naver.com>
2024-01-29 18:11:10 +0900

    feat: 축하 게시글(오늘의 기념일) 자동 작성 기능 구현

--- src/main/java/com/ssginc/secretgarden/domain/member/repository/MemberRepository.java
+++ src/main/java/com/ssginc/secretgarden/domain/member/repository/MemberRepository.java
@@ -2,7 +2,10 @@ package com.ssginc.secretgarden.domain.member.repository;
 
 import com.ssginc.secretgarden.domain.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Integer> {
@@ -10,5 +13,8 @@ public interface MemberRepository extends JpaRepository<Member, Integer> {
     public Member findByBlossomId(String blossomId);
 
     Optional<Member> findByBlossomIdAndPassword(String blossomId, String password);
+
+    @Query("SELECT m FROM Member m WHERE FUNCTION('MONTH', m.birthDate) = :month AND FUNCTION('DAY', m.birthDate) = :day")
+    List<Member> findByMonthAndDay(@Param("month") int month, @Param("day") int day);
 
 }
